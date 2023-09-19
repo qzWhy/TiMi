7f8505d2fe2d928043485b0a624e378b20bc2cbc
Why <745680110@qq.com>
2023-09-19 15:52:48 +0800

    commiy

---
+++ Pods/HHTransition/HHTransition/HHPresentFlipTransition.m
@@ -0,0 +1,159 @@
+//
+//  HHPresentFlipTransition.m
+//  https://github.com/yuwind/HHTransition
+//
+//  Created by 豫风 on 2020/4/13.
+//  Copyright © 2020 豫风. All rights reserved.
+//
+
+#import "HHPresentFlipTransition.h"
+#import <objc/runtime.h>
+#import "UIViewController+HHTransitionProperty.h"
+
+@interface UIViewController (translucentView)
+
+@property (nonatomic, strong) UIView *translucentView_;
+
+@end
+
+@implementation UIViewController (translucentView)
+
+- (UIView *)translucentView_ {
+    UIView *translucentView_ = objc_getAssociatedObject(self, @selector(translucentView_));
+    if (!translucentView_) {
+        CGFloat alpha = self.translucentViewAlpha > 0 ? self.translucentViewAlpha : 0.8;
+        translucentView_ = [[UIView alloc] init];
+        translucentView_.backgroundColor = [UIColor colorWithWhite:0 alpha:alpha];
+        self.translucentView_ = translucentView_;
+    }
+    return translucentView_;
+}
+
+- (void)setTranslucentView_:(UIView *)translucentView_ {
+    objc_setAssociatedObject(self, @selector(translucentView_), translucentView_, OBJC_ASSOCIATION_RETAIN);
+}
+
+@end
+
+@interface HHPresentFlipTransition ()
+
+@property (nonatomic, assign) HHPresentStyle style;
+
+@end
+
+@implementation HHPresentFlipTransition
+
++ (instancetype)flipTransitionWithStyle:(HHPresentStyle)style isBegining:(BOOL)isBegining {
+    
+    HHPresentFlipTransition *flipTransition = [HHPresentFlipTransition transitionWithIsBegining:isBegining];
+    flipTransition.style = style;
+    
+    return flipTransition;
+}
+
+- (void)beginTransitionWithTransitionContext:(id<UIViewControllerContextTransitioning>)transitionContext {
+    UIViewController *toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    UIView *translucentView = toVC.translucentView_;
+    
+    UIView *containerView = [transitionContext containerView];
+    UIView *toView = [transitionContext viewForKey:UITransitionContextToViewKey];
+    
+    toView.frame = containerView.bounds;
+    translucentView.frame = containerView.bounds;
+    [containerView addSubview:translucentView];
+    [containerView addSubview:toView];
+    
+    [self setupBeginInfoWithToView:toView];
+    translucentView.alpha = 0;
+    CGFloat duration = [self transitionDuration:transitionContext];
+    
+    CGFloat damping = 1;
+    if (self.style == HHPresentStyleSlipFromTop) {
+        damping = 0.65;
+    }
+    [UIView animateWithDuration:duration delay:0 usingSpringWithDamping:damping initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        translucentView.alpha = 1;
+        [self setupSuspendInfoWithToView:toView];
+    } completion:^(BOOL finished) {
+        [transitionContext completeTransition:YES];
+    }];
+}
+
+- (void)endTransitionWithTransitionContext:(id<UIViewControllerContextTransitioning>)transitionContext {
+    UIViewController *fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    UIView *translucentView = fromVC.translucentView_;
+    
+    UIView *fromView = [transitionContext viewForKey:UITransitionContextFromViewKey];
+    CGFloat duration = [self transitionDuration:transitionContext];
+    
+    CGFloat damping = 1;
+    if (self.style == HHPresentStyleSlipFromTop) {
+        damping = 0.65;
+    }
+    [UIView animateWithDuration:duration delay:0 usingSpringWithDamping:damping initialSpringVelocity:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        translucentView.alpha = 0;
+        [self setupEndInfoWithFromView:fromView];
+    } completion:^(BOOL finished) {
+        [transitionContext completeTransition:YES];
+        [translucentView removeFromSuperview];
+    }];
+}
+
+- (void)setupBeginInfoWithToView:(UIView *)toView {
+    switch (self.style) {
+        case HHPresentStyleSlipFromTop:
+            toView.hh_y = -toView.hh_height;
+            break;
+        case HHPresentStyleSlipFromBottom:
+            toView.hh_y = toView.hh_height;
+            break;
+        case HHPresentStyleSlipFromLeft:
+            toView.hh_x = -toView.hh_width;
+            break;
+        case HHPresentStyleSlipFromRight:
+            toView.hh_x = toView.hh_width;
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)setupSuspendInfoWithToView:(UIView *)toView {
+    switch (self.style) {
+        case HHPresentStyleSlipFromTop:
+            toView.hh_y = 0;
+            break;
+        case HHPresentStyleSlipFromBottom:
+            toView.hh_y = 0;
+            break;
+        case HHPresentStyleSlipFromLeft:
+            toView.hh_x = 0;
+            break;
+        case HHPresentStyleSlipFromRight:
+            toView.hh_x = 0;
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)setupEndInfoWithFromView:(UIView *)fromView {
+    switch (self.style) {
+        case HHPresentStyleSlipFromTop:
+            fromView.hh_y = fromView.hh_height;
+            break;
+        case HHPresentStyleSlipFromBottom:
+            fromView.hh_y = fromView.hh_height;
+            break;
+        case HHPresentStyleSlipFromLeft:
+            fromView.hh_x = fromView.hh_width;
+            break;
+        case HHPresentStyleSlipFromRight:
+            fromView.hh_x = -fromView.hh_width;
+            break;
+        default:
+            break;
+    }
+}
+
+@end
---
+++ Pods/HHTransition/HHTransition/UIViewController+HHTransitionProperty.h
@@ -0,0 +1,27 @@
+//
+//  UIViewController+HHTransitionProperty.h
+//  https://github.com/yuwind/HHTransition
+//
+//  Created by 豫风 on 2020/4/18.
+//  Copyright © 2020 豫风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HHTransitionUtility.h"
+#import "HHInteractionDelegate.h"
+#import "HHTransitioningDelegate.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIViewController (HHTransitionProperty)
+
+@property (nonatomic, assign, readwrite) HHPresentStyle presentStyle;
+@property (nonatomic, assign, readwrite) HHPushStyle pushStyle;
+@property (nonatomic, assign, readwrite) CGFloat translucentViewAlpha;
+@property (nonatomic, strong, readwrite) HHTransitioningDelegate *transitionDelegate;
+@property (nonatomic, strong, readwrite) HHInteractionDelegate *interactionDelegate;
+@property (nonatomic, strong) UIView *translucentView;
+
+@end
+
+NS_ASSUME_NONNULL_END
